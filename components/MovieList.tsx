b728239f719d04f5f2cd4c4fb4c79ed744097914
Les Crow <john.baruteau@gmail.com>
2023-02-21 14:36:34 +0100

    no image available

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { releaseDate } from "../utils/constants";
+import { releaseDate, srcImage } from "../utils/constants";
 import { DirectorProps, MovieProps } from "../utils/globalTypes";
 
 type Props = {
@@ -9,15 +9,15 @@ type Props = {
 
 export default function MovieList({ movie }: Props) {
   return (
-    <div className="flex border mb-4 p-4">
+    <div className="flex border border-black mb-4 p-4">
       <Image
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        src={srcImage(movie.poster_path)}
         width={100}
         height={150}
         alt={movie.title}
       />
       <div className="w-full text-center">
-        <h1>{movie.title}</h1>
+        <h1 className="text-xl">{movie.title}</h1>
         <p>{releaseDate(movie.release_date)}</p>
         <p>{movie.vote_average}</p>
       </div>
