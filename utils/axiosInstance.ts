84258df106faf5f9f649982b8217a4bb4c40ebc9
Les Crow <john.baruteau@gmail.com>
2023-02-24 19:33:52 +0100

    base url

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const fetchClient = () => {
   const defaultOptions = {
-    baseURL: process.env.NEXT_PUBLIC_SERVER_URL || "SERVER_URL",
+    baseURL:
+      process.env.NEXT_PUBLIC_SERVER_URL_DEVELOPPMENT ||
+      process.env.NEXT_PUBLIC_URL,
   };
 
   // Create instance
